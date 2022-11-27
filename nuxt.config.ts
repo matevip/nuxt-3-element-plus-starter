9d898081866c257c123e160f9cf61df83cb14a8c
matevip <7333791@qq.com>
2022-11-27 22:10:36 +0800

    doc: 修改注释描述内容

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,10 @@
 export default defineNuxtConfig({
   modules: ["nuxt-windicss", '@pinia/nuxt'],
 
-  // 引入ant-design-vue样式
+  // 引入elemnet plus样式
   css: ['element-plus/dist/index.css', 'element-plus/theme-chalk/display.css'],
 
-  // 插件方式引入ant-design-vue组件
+  // 插件方式引入element plus组件
   plugins: ["@/plugins/element-plus"],
 
   vite: {
